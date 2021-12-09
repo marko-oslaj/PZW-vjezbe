b7b56d86c88daf10506dd88fb175f1ddc2d4763d
marko-oslaj <93705378+marko-oslaj@users.noreply.github.com>
2021-12-09 11:51:59 +0100

    Add files via upload

---
+++ vjezba02dogadajinawebstranici/script.js
@@ -0,0 +1,12 @@
+
+function datum() {
+
+    document.getElementById("unos").innerHTML = Date();
+
+}
+
+function naslov() {
+
+    document.getElementById("naslov").innerHTML = "Woow ovo je novi naslov!";
+
+}
